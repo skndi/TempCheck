560a784ca862fbdced85e21eed8d5870db5d25de
Radostin Marinov <radostinmar21@gmail.com>
2022-12-24 15:46:31 +0200

    Code refactoring

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
